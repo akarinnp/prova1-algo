595dd97a9273e01499014dbd5ff144232837c157
Luana Karoline Lima Costa <58597314+akarinnp@users.noreply.github.com>
2023-06-21 00:34:09 -0300

    Update index.js

--- exer-03/index.js
+++ exer-03/index.js
@@ -1,19 +1,19 @@
-let btn = document.getElementById('btn')
+
 let div = document.getElementById('resultado')
 let soma = 0
 
 let num1 = prompt('digite um número:')
 let num2 = prompt('digite um número:')
 
-if(num1 < 0 && num2 < 0){
+if(num1 < 0 && num1 > 100 num2 < 0 && num2 > 100){
     alert('ERRO-digite um numero maior que 0')
     
 
 }
 else{
-   for(let i = num1 ;i < num2; i++ ){
+   for(let i = num1 ; i <= num2; i++ ){
     if(i % 2 != 0){
-        soma+=i
+        soma += i
 
     }
     
