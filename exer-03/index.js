79a6df196dd0fc9d5e125718698ee9f28eed9b47
Luana Karoline Lima Costa <58597314+akarinnp@users.noreply.github.com>
2023-06-21 00:55:37 -0300

    Update index.js

--- exer-03/index.js
+++ exer-03/index.js
@@ -2,20 +2,20 @@ let number1 = document.getElementById('numero1');
 let number2 = document.getElementById('numero2');
 let btn = document.getElementById('btn');
 let div = document.getElementById('result');
+    let soma = 0;
 
 function somaImpares(){
     let num1 = parseInt(number1.value);
     let num2 = parseInt(number2.value);
-    let i = 0;
-    let soma = 0;
+    
     
 if(num1 < 0 && num2 < 0){
-    alert('ERRO-digite um numero maior que 0')
+  document.write('ERRO-digite um numero maior que 0')
     
 
 }
 else{
-   for(i = num1 ;i < num2; i++ ){
+   for(let i = num1 ;i < num2; i++ ){
     if(i % 2 != 0){
         soma+=i
 
