89dfc4cfb05849f58f5bde0da5e17d34aa4b1ade
Luana Karoline Lima Costa <58597314+akarinnp@users.noreply.github.com>
2023-06-21 00:39:37 -0300

    Update index.js

--- exer-03/index.js
+++ exer-03/index.js
@@ -5,19 +5,18 @@ let soma = 0
 let num1 = prompt('digite um número:')
 let num2 = prompt('digite um número:')
 
-if(num1 < 0 && num1 > 100 num2 < 0 && num2 > 100){
+if(num1 < 0 && num2 < 0){
     alert('ERRO-digite um numero maior que 0')
     
 
 }
 else{
-   for(let i = num1 ; i <= num2; i++ ){
+   for(let i = num1 ;i < num2; i++ ){
     if(i % 2 != 0){
-        soma += i
+        soma+=i
 
     }
     
 }
 div.innerHTML= `a soma entre os numero ${num1} e ${num2} é: ${soma}`
 }
-
